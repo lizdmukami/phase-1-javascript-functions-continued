fb3132df26d2a2f2035fb0111708a40c1159f4ca
Moringa School <lizdmukami@gmail.com>
2022-11-23 04:36:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(watch = 'roller-skate'){
+    return `This Saturday, I want to ${watch}!`;
+    }
+    saturdayFun();
+
+const mondayWork = function (work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+mondayWork();
+
+function wrapAdjective(superb ="*" ) {
+    return function (param = "special") {
+      const encouragingPromptFunction = wrapAdjective("!!!")
+            return `You are ${superb}${param}${superb}!`;
+    }
+}
+console.log(wrapAdjective("%")("a dedicated programmer"));
